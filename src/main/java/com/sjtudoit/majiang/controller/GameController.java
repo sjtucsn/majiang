12bb1fcf374b08702a64f4467c9774117d23a97e
sjtucsn <570979438@qq.com>
2019-09-02 20:02:58 +0800

    取消websocket连接出错时关闭会话的逻辑，会造成userMap清理不干净

--- src/main/java/com/sjtudoit/majiang/controller/GameController.java
+++ src/main/java/com/sjtudoit/majiang/controller/GameController.java
@@ -658,8 +658,7 @@ public class GameController {
     }
 
     @OnError
-    public void onError(Session session, Throwable error) throws Exception{
-        session.close();
+    public void onError(Session session, Throwable error){
         LOGGER.error("websocket连接出错", error);
     }
 
