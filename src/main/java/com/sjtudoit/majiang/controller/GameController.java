7f1e405483a659610e19df8b40d7f79b764e8445
sjtucsn <570979438@qq.com>
2020-01-27 20:58:05 +0800

    添加防止误操作代码(fix bug)

--- src/main/java/com/sjtudoit/majiang/controller/GameController.java
+++ src/main/java/com/sjtudoit/majiang/controller/GameController.java
@@ -262,7 +262,7 @@ public class GameController {
         Game currentGame = currentGameList.get(tableId);
 
         // 防止不同用户之间的误操作，只有抢金指令可以无视当前轮到用户
-        if (!receivedMessage.getMessage().equals("抢金") && currentGame.getCurrentUserName() != null && !currentGame.getCurrentUserName().equals(sessionUserName)) {
+        if (!Objects.equals(receivedMessage.getMessage(), "抢金") && !Objects.equals(currentGame.getCurrentUserName(), sessionUserName)) {
             LOGGER.info("指令无效，此时轮到的不是{}", sessionUserName);
             return;
         }
